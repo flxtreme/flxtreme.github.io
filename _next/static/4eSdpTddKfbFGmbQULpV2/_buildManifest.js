ca144816587cbdd918f5aca094331a9c7a4c8c70
flxtreme <flxtreme@users.noreply.github.com>
2025-11-02 07:15:54 +0000

    deploy: 5d6ea552d88c56d610960053677659c191a5ac3e

--- _next/static/4eSdpTddKfbFGmbQULpV2/_buildManifest.js
+++ _next/static/4eSdpTddKfbFGmbQULpV2/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/flxtreme.github.io//_next/:path+",
+        "destination": "/flxtreme.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
