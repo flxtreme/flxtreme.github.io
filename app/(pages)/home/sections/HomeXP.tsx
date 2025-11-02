5f09c54680699878ff02327868c6f02ca10e5435
Felix Ruz <flxrzjr@gmail.com>
2025-11-02 17:34:15 +0800

    fixes

--- app/(pages)/home/sections/HomeXP.tsx
+++ app/(pages)/home/sections/HomeXP.tsx
@@ -4,6 +4,7 @@ import { useProjects } from "@/app/(shared)/hooks/useProjects";
 import { cn } from "@/lib/utils";
 import { FiEye, FiEyeOff, FiLock } from "react-icons/fi";
 import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
 export default function HomeXP() {
   const { projects, showPrivate, setShowPrivate } = useProjects();
