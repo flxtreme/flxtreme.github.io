926afbfb9273a01e39f2072200da9a4fb312935e
Felix Ruz <flxrzjr@gmail.com>
2025-11-02 18:01:56 +0800

    fix nav

--- app/(shared)/constants/index.ts
+++ app/(shared)/constants/index.ts
@@ -1,12 +1,11 @@
 import { NavType } from "@/app/(shared)/types";
-import { IoMdContact } from "react-icons/io";
-import { IoBookOutline, IoCallOutline, IoChatbubbleEllipsesOutline, IoChatbubbleOutline, IoCodeOutline, IoLogoWebComponent, IoMailOpenOutline } from "react-icons/io5";
+import { IoBookOutline, IoChatbubbleEllipsesOutline, IoCodeOutline, IoLogoWebComponent } from "react-icons/io5";
 
 
 
 export const NAV_ITEMS: NavType[] = [
-  { label: "Skills", href: "#skills", icon: IoCodeOutline },
-  { label: "Projects", href: "#projects", icon: IoLogoWebComponent },
-  { label: "Hire Me", href: "#hire-me", icon: IoChatbubbleEllipsesOutline },
+  { label: "Skills", href: "/#skills", icon: IoCodeOutline },
+  { label: "Projects", href: "/#projects", icon: IoLogoWebComponent },
+  { label: "Hire Me", href: "/#hire-me", icon: IoChatbubbleEllipsesOutline },
   { label: "My Blogs", href: "/blog", icon: IoBookOutline }
 ]
