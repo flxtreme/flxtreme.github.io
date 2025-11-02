bd841eb3e560609010c0b05da35ed5b0937f8a30
Felix Ruz <flxrzjr@gmail.com>
2025-11-02 15:45:17 +0800

    fix slider

--- app/(pages)/home/sections/HomeXP.tsx
+++ app/(pages)/home/sections/HomeXP.tsx
@@ -28,8 +28,7 @@ export default function HomeXP() {
     arrows: false,
     pauseOnHover: true,
     responsive: [
-      { breakpoint: 1536, settings: { slidesToShow: 3 } },
-      { breakpoint: 1280, settings: { slidesToShow: 3 } },
+      { breakpoint: 1280, settings: { slidesToShow: 2 } },
       { breakpoint: 1024, settings: { slidesToShow: 2 } },
       { breakpoint: 768, settings: { slidesToShow: 1 } },
       { breakpoint: 500, settings: { slidesToShow: 1 } },
