b97721f04b09010d1b9691d39c6584930ab25c69
Felix Ruz <flxrzjr@gmail.com>
2025-11-02 15:17:22 +0800

    fix path

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // Add these for GitHub Pages
-  basePath: '/flxtreme.github.io',        // replace with your repo name
-  assetPrefix: '/flxtreme.github.io/',    // ensures assets load correctly
+  basePath: '/flxtreme.github.io'
 };
 
 export default nextConfig;
